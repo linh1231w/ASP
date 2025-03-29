2a1e9f5b1716d91b653ef828f54d1c0411851552
linh1231w <nmylinh1234567@gmail.com>
2025-03-29 11:13:47 +0700

    Cập nhật tính năng

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NguyenthimylinhB2.Model;
+
+namespace NguyenthimylinhB2.Data
+{
+    public class AppDbContext : DbContext // Kế thừa DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; } // Thêm bảng Category
+    }
+}
--- Model/Product.cs
+++ Model/Product.cs
@@ -2,5 +2,9 @@
 {
     public class Product
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public string Image { get; set; }
     }
 }
